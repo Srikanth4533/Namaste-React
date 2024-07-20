1a743675acb25156b798c925bbb2a30aacfb8ce3
Srikanth4533 <kondakallasrikanth991@gmail.com>
2024-07-20 10:27:08 +0530

    created a nested parent child structure using React create Element

--- App.js
+++ App.js
@@ -1,11 +1,25 @@
-const heading = React.createElement(
-  "h1",
-  { id: "heading", xyz: "abc" },
-  "Hello world from React"
+/**
+ *
+ * <div id="parent">
+ *     <div id="child">
+ *         <h1>I'm an h1 tag</h1>
+ *     </div>
+ * </div>
+ *
+ */
+
+const parent = React.createElement(
+  "div",
+  { id: "parent" },
+  React.createElement(
+    "div",
+    { id: "child" },
+    React.createElement("h1", {}, "I'm an h1 tag")
+  )
 );
 
-console.log(heading);
+console.log(parent);
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 
-root.render(heading);
+root.render(parent);
