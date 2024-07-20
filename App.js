fed76f8dc4d66826fbee5ea825eba7f3b558c0f6
Srikanth4533 <kondakallasrikanth991@gmail.com>
2024-07-20 10:30:07 +0530

    nested two children structure

--- App.js
+++ App.js
@@ -9,14 +9,16 @@
  *
  */
 
-const parent = React.createElement(
-  "div",
-  { id: "parent" },
-  React.createElement("div", { id: "child" }, [
+const parent = React.createElement("div", { id: "parent" }, [
+  React.createElement("div", { id: "child 1" }, [
     React.createElement("h1", {}, "I'm an h1 tag"),
     React.createElement("h2", {}, "I'm an h2 tag"),
-  ])
-);
+  ]),
+  React.createElement("div", { id: "child 2" }, [
+    React.createElement("h1", {}, "I'm an h1 tag"),
+    React.createElement("h2", {}, "I'm an h2 tag"),
+  ]),
+]);
 
 console.log(parent);
 
