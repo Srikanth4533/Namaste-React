fef1935f5a8abb6fd9d8a2b9771f396fc13757e1
Srikanth4533 <kondakallasrikanth991@gmail.com>
2024-07-23 14:46:51 +0530

    Configured react and react-dom

--- App.js
+++ App.js
@@ -1,3 +1,6 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+
 /**
  *
  * <div id="parent">
@@ -11,7 +14,7 @@
 
 const parent = React.createElement("div", { id: "parent" }, [
   React.createElement("div", { id: "child 1" }, [
-    React.createElement("h1", {}, "I'm an h1 tag"),
+    React.createElement("h1", {}, "Namaste React is Live!"),
     React.createElement("h2", {}, "I'm an h2 tag"),
   ]),
   React.createElement("div", { id: "child 2" }, [
